362594386d7a6e29e8c83ac41e935961dd192874
bit-ku2025 <mdimasridho909@gmail.com>
2026-09-13 15:38:23 +0700

    feat: Hardcore 100:100 Zero-Compromise Zygisk Shield & suSFS v2.2 integration

--- jni/zygisk_module.cpp
+++ jni/zygisk_module.cpp
@@ -2,16 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <android/log.h>
 #include "zygisk.hpp"
-
-#define LOG_TAG "RootAssistantBypass"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 
-class RootAssistantUltimate : public zygisk::ModuleBase {
+class RootAssistantHardcore : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
         this->api = api;
@@ -24,30 +20,25 @@ public:
         const char *process_name = env->GetStringUTFChars(args->nice_name, nullptr);
         if (!process_name) return;
 
-        // Targetkan BCA Mobile dan aplikasi finansial lainnya
         if (strstr(process_name, "com.bca") || 
             strstr(process_name, "ovo.id") || 
-            strstr(process_name, "alfamart")) {
+            strstr(process_name, "alfamart") ||
+            strstr(process_name, "topjohnwu") ||
+            strstr(process_name, "security") ||
+            strstr(process_name, "momo") ||
+            strstr(process_name, "ami") ||
+            strstr(process_name, "root")) {
             
             api->setOption(zygisk::Option::FORCE_DENYLIST_UNMOUNT);
-            LOGI("Target hooked & unmounted for process: %s", process_name);
         }
 
         env->ReleaseStringUTFChars(args->nice_name, process_name);
     }
 
     void postAppSpecialize(const AppSpecializeArgs *args) override {
-        if (!args || !args->nice_name) return;
-        const char *process_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        if (!process_name) return;
-
-        // Jika proses adalah BCA Mobile, lakukan spoofing variabel lingkungan dan properti runtime
-        if (strstr(process_name, "com.bca")) {
-            setenv("ACCESSIBILITY_ENABLED", "0", 1);
-            LOGI("Neutralizing Accessibility checks for BCA");
-        }
-
-        env->ReleaseStringUTFChars(args->nice_name, process_name);
+        setenv("ACCESSIBILITY_ENABLED", "0", 1);
+        setenv("RO_DEBUGGABLE", "0", 1);
+        setenv("RO_SECURE", "1", 1);
     }
 
 private:
@@ -55,4 +46,4 @@ private:
     JNIEnv *env;
 };
 
-REGISTER_ZYGISK_MODULE(RootAssistantUltimate)
+REGISTER_ZYGISK_MODULE(RootAssistantHardcore)
