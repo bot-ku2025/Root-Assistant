978362c5bfb5fce9788a65946559c52df57f6d7d
bit-ku2025 <mdimasridho909@gmail.com>
2026-09-13 21:32:01 +0700

    feat: ultimate level 100 JNI memory reflection hack to force disable accessibility

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,17 +1,49 @@
 #include <jni.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <android/log.h>
 #include "zygisk.hpp"
 
 #define LOG_TAG "RootAssistantDeepShield"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
+JavaVM *g_jvm = nullptr;
+
+// Thread siluman untuk meretas memori Java saat aplikasi berjalan
+void* shield_thread(void* arg) {
+    JNIEnv *env = nullptr;
+    // Kaitkan thread C++ ke mesin Virtual Java (JVM)
+    if (g_jvm->AttachCurrentThread(&env, nullptr) != JNI_OK) return nullptr;
+
+    // Cari kelas AccessibilityManager di memori
+    jclass accClass = env->FindClass("android/view/accessibility/AccessibilityManager");
+    if (accClass != nullptr) {
+        jfieldID mIsEnabledField = env->GetFieldID(accClass, "mIsEnabled", "Z");
+        jfieldID sInstanceField = env->GetStaticFieldID(accClass, "sInstance", "Landroid/view/accessibility/AccessibilityManager;");
+
+        // Polling loop: Paksa nilai Accessibility menjadi FALSE di dalam RAM aplikasi selama 10 detik awal
+        for (int i = 0; i < 100; i++) { 
+            if (sInstanceField != nullptr && mIsEnabledField != nullptr) {
+                jobject sInstance = env->GetStaticObjectField(accClass, sInstanceField);
+                if (sInstance != nullptr) {
+                    // Eksekusi peretasan nilai boolean (FALSE)
+                    env->SetBooleanField(sInstance, mIsEnabledField, JNI_FALSE);
+                }
+            }
+            usleep(100000); // Tembak setiap 100 milidetik agar BCA tertipu
+        }
+    }
+    
+    g_jvm->DetachCurrentThread();
+    return nullptr;
+}
+
 class RootAssistantHook : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
         this->api = api;
-        this->env = env;
+        env->GetJavaVM(&g_jvm);
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
@@ -19,21 +51,15 @@ public:
     }
 
     void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
-        JNIEnv *env = nullptr;
-        JavaVM *vm = api->getJavaVM();
-        if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
-            if (vm->AttachCurrentThread(&env, nullptr) != JNI_OK) {
-                return;
-            }
-        }
-
-        // Hooking runtime environment untuk menetralisir pemeriksaan aksesibilitas dan root
-        LOGD("RootAssistantDeepShield: Injecting runtime shields into app process.");
+        // Luncurkan thread siluman secara paralel saat aplikasi target dibuka
+        pthread_t thread;
+        pthread_create(&thread, nullptr, shield_thread, nullptr);
+        pthread_detach(thread);
+        LOGD("RootAssistantDeepShield: Memory overriding thread started in app process.");
     }
 
 private:
     zygisk::Api *api;
-    JNIEnv *env;
 };
 
 REGISTER_ZYGISK_MODULE(RootAssistantHook)
