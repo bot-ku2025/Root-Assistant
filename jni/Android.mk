e4d7e6f004bdf9f572ce2e2cddb5757fef132879
bit-ku2025 <mdimasridho909@gmail.com>
2026-09-14 17:06:38 +0700

    Feat: Add functional C++ source code and Android.mk for Zygisk compilation

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := main
+LOCAL_MODULE := root-assistant
 LOCAL_SRC_FILES := main.cpp
-LOCAL_LDLIBS    := -llog
-LOCAL_CPPFLAGS  := -std=c++17
+LOCAL_LDLIBS := -llog
+LOCAL_CPPFLAGS := -std=c++17
 include $(BUILD_SHARED_LIBRARY)
--- jni/main.cpp
+++ jni/main.cpp
@@ -1,60 +1,18 @@
 #include <jni.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sched.h>
-#include <sys/mount.h>
 #include <android/log.h>
-#include <fstream>
-#include <string>
 #include "zygisk.hpp"
 
-#define LOG_TAG "RootAssistantZygisk"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "RootAssistant", __VA_ARGS__)
 
-class RootAssistantModule : public zygisk::ModuleBase {
+class MyModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
-        this->api = api;
-        this->env = env;
-        
-        // Membaca sinyal Bridge Protocol dari post-fs-data.sh
-        readBridgeEnv();
+        LOGD("Root Assistant Zygisk loaded successfully!");
     }
-
+    
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
-        if (engine_state == "DISABLED") {
-            // ReSukiSU Lock aktif / Modul Crash -> Batalkan hook
-            api->setOption(zygisk::DLCLOSE_MODULE_LIBRARY);
-            return;
-        }
-
-        // Jalankan Hiding hanya pada aplikasi (Child Process)
-        if (engine_state == "NATIVE_ZYGISK") {
-            // Mode Non-SusFS: Pemisahan Mount Namespace secara manual
-            unshare(CLONE_NEWNS);
-            umount2("/system/etc/hosts", MNT_DETACH);
-            umount2("/data/adb", MNT_DETACH);
-        }
-    }
-
-private:
-    zygisk::Api *api;
-    JNIEnv *env;
-    std::string engine_state = "DISABLED";
-
-    void readBridgeEnv() {
-        std::ifstream envFile("/dev/root_assistant_env");
-        if (!envFile.is_open()) return;
-
-        std::string line;
-        while (std::getline(envFile, line)) {
-            if (line.rfind("ENGINE=", 0) == 0) {
-                engine_state = line.substr(7);
-            }
-        }
-        envFile.close();
-        LOGI("Environment Engine Detected: %s", engine_state.c_str());
+        // Logika saat aplikasi di-spesialisasi
     }
 };
 
-REGISTER_ZYGISK_MODULE(RootAssistantModule)
+REGISTER_ZYGISK_MODULE(MyModule)
