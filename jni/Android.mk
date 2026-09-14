6bd914656454f75b0035703c114e4add261c6f3c
bit-ku2025 <mdimasridho909@gmail.com>
2026-09-14 15:25:34 +0700

    Feat (Tahap 2): Add Zygisk C++ native logic with bridge protocol reader

---
+++ jni/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := main
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS    := -llog
+LOCAL_CPPFLAGS  := -std=c++17
+include $(BUILD_SHARED_LIBRARY)
