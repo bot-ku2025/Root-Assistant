cc0654d0e0944cccbfa848b0c5929f8489cf4e20
bit-ku2025 <mdimasridho909@gmail.com>
2026-09-14 17:40:27 +0700

    Fix: Remove external curl header dependency and use self-contained Zygisk structure

--- jni/main.cpp
+++ jni/main.cpp
@@ -2,18 +2,64 @@
 #include <android/log.h>
 #include <unistd.h>
 #include <sys/system_properties.h>
-#include "zygisk.hpp"
 
 #define LOG_TAG "RootAssistant"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+// Definisi Standar Zygisk API langsung (Self-contained implementation)
+namespace zygisk {
+struct Api;
+struct AppSpecializeArgs {
+    jint *uid;
+    jint *gid;
+    jobjectArray *gids;
+    jint *runtime_flags;
+    jobjectArray *rlimits;
+    jint *mount_external;
+    jstring *se_info;
+    jstring *nice_name;
+    jstring *instruction_set;
+    jstring *app_data_dir;
+};
+struct ServerSpecializeArgs {
+    jint *uid;
+    jint *gid;
+    jobjectArray *gids;
+    jint *runtime_flags;
+    jobjectArray *rlimits;
+    jint *permitted_capabilities;
+    jobjectArray *effective_capabilities;
+};
+class ModuleBase {
+public:
+    virtual ~ModuleBase() {}
+    virtual void onLoad(Api *api, JNIEnv *env) {}
+    virtual void preAppSpecialize(AppSpecializeArgs *args) {}
+    virtual void postAppSpecialize(const AppSpecializeArgs *args) {}
+    virtual void preServerSpecialize(ServerSpecializeArgs *args) {}
+    virtual void postServerSpecialize(const ServerSpecializeArgs *args) {}
+};
+struct Api {
+    enum Option {
+        PATCH_DLOPEN = 0,
+        DLCLOSE_SKIP_FINI = 1
+    };
+    virtual void *pltHookRegister(const char *lib, const char *symbol, void *new_func, void **old_func) = 0;
+    virtual void *pltHookCommit() = 0;
+    virtual void setOption(Option opt) = 0;
+    virtual void *connectCompanion() = 0;
+    virtual void registerModule(ModuleBase *module) = 0;
+    static void init(Api *api, JNIEnv *env) {}
+};
+} // namespace zygisk
 
 class RootAssistantModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
         this->api = api;
         this->env = env;
-        LOGI("RootAssistant loaded successfully.");
+        LOGI("RootAssistant Self-Contained Core loaded successfully.");
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
@@ -22,18 +68,15 @@ public:
             if (nice_name && env) {
                 const char *name = env->GetStringUTFChars(nice_name, nullptr);
                 if (name) {
-                    LOGD("Target App -> %s", name);
+                    LOGD("Target App Detected -> %s", name);
                     env->ReleaseStringUTFChars(nice_name, name);
                 }
             }
         }
     }
 
-    void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
-    }
-
     void preServerSpecialize(zygisk::ServerSpecializeArgs *args) override {
-        LOGI("System server specialized.");
+        LOGI("System server specialization secured.");
     }
 
 private:
@@ -41,13 +84,12 @@ private:
     JNIEnv *env = nullptr;
 };
 
-// Pendaftaran entry point Zygisk yang kompatibel dengan standar header modern
+// Ekspor entry point Zygisk secara mandiri
 static void register_module(zygisk::Api *api) {
     zygisk::ModuleBase *module = new RootAssistantModule();
     api->registerModule(module);
 }
 
-// Ekspor fungsi entry point Zygisk secara eksplisit
 extern "C" {
 __attribute__((visibility("default"))) __attribute__((used))
 void zygisk_module_entry(zygisk::Api *api, JNIEnv *env) {
