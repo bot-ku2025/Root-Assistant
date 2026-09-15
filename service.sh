a99c7c2c91154d280eda3213bae2942b4b83abf3
bit-ku2025 <mdimasridho909@gmail.com>
2026-09-15 23:10:43 +0700

    fix: remove ModuleSentinel hiding

--- service.sh
+++ service.sh
@@ -5,11 +5,6 @@ until [ "$(getprop sys.boot_completed)" = "1" ]; do
 done
 sleep 3
 
-# Sembunyikan folder jebakan Sentinel secara universal di level kernel
-TARGET_TRAP="/data/adb/ModuleSentinel"
-if [ -d "$TARGET_TRAP" ]; then
-    mount -t tmpfs -o size=4k tmpfs "$TARGET_TRAP"
-fi
 
 # Optimasi izin akses path dasar agar tidak mudah ditelusuri oleh direktori traversal biasa
 if [ -d "/data/adb" ]; then
